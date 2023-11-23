6ad2fd0597f68aebcf7914223d8a9e320d5c060c
huynbgithub <nguyenbahuytk@gmail.com>
2023-11-23 13:04:50 +0700

    game mechanics completed

--- BigPicture.sol
+++ BigPicture.sol
@@ -10,20 +10,22 @@ import {IVRFCoordinator} from "@bisonai/orakl-contracts/src/v0.1/interfaces/IVRF
 
 contract BigPicture is VRFConsumerBase, ERC721URIStorage, Ownable {
 
-    event ReceiveCalled(uint amount);
+    // event ReceiveCalled(uint amount);
 
     receive() external payable {
-        emit ReceiveCalled(msg.value);
+        // emit ReceiveCalled(msg.value);
     }
 
     IVRFCoordinator COORDINATOR;
-    uint64 public sAccountId;
-    bytes32 public sKeyHash;
-    uint32 public sCallbackGasLimit = 300000;
-    uint32 public sNumWords = 1;
+    uint64 sAccountId;
+    bytes32 sKeyHash;
+    uint32 sCallbackGasLimit = 300000;
+    uint32 sNumWords = 1;
     uint public randomIndex;
 
-    uint256 private _nextTokenId;
+    uint256 public _nextTokenId;
+    mapping (uint256 => bool) public onSale;
+    mapping (uint256 => uint256) public tokenPrice;
 
     string public bigPictureName;
     string public image;
@@ -66,7 +68,7 @@ contract BigPicture is VRFConsumerBase, ERC721URIStorage, Ownable {
 
         for (uint256 i = 0; i < _nextTokenId; i++) {
             if (ownerOf(i) == owner) {
-                tokenList[i] = TokenData(i, tokenURI(i));
+                tokenList[i] = TokenData(i, tokenURI(i), onSale[i], tokenPrice[i]);
             }
         }
         return tokenList;
@@ -89,15 +91,63 @@ contract BigPicture is VRFConsumerBase, ERC721URIStorage, Ownable {
         uint256 tokenId = _nextTokenId++;
         _mint(tempAddress, tokenId);
         _setTokenURI(tokenId, picturePieces[randomIndex]);
+        onSale[tokenId] = false;
+        tokenPrice[tokenId] = 0;
     }
 
     address public tempAddress;
 
     function mintCMT () public payable {
-        require(msg.value == mintPrice, "Amount must equal mint price!");
         tempAddress = msg.sender;
-        payable(address(this)).transfer(msg.value);
+        payable(address(this)).transfer(mintPrice);
         requestRandomWords();
+    }
+
+    function mintCMTDemo() public payable {
+        tempAddress = msg.sender;
+        payable(address(this)).transfer(mintPrice * 9);
+        for (uint256 i = 0; i < picturePieces.length; i++) {
+            uint256 tokenId = _nextTokenId++;
+            _mint(tempAddress, tokenId);
+            _setTokenURI(tokenId, picturePieces[i]);
+            onSale[tokenId] = false;
+            tokenPrice[tokenId] = 0;
+        }
+    }
+
+    function putTokenOnSale(uint256 id, uint256 price) public {
+        onSale[id] = true;
+        tokenPrice[id] = price;
+    }
+
+    function getTokensOnSale () public view returns (TokenData[] memory) {
+        TokenData[] memory tokenList = new TokenData[](_nextTokenId);
+
+        uint256 tokenCount = 0;
+
+        for (uint256 i = 0; i < _nextTokenId; i++) {
+            if (onSale[i] == true && ownerOf(i) != msg.sender) {
+                tokenList[tokenCount] = TokenData(i, tokenURI(i), onSale[i], tokenPrice[i]);
+                tokenCount++;
+            }
+        }
+        return tokenList;
+    }
+
+    function purchaseToken(uint256 tokenId) public {
+        payable(msg.sender).transfer(tokenPrice[tokenId]);
+        payable(ownerOf(tokenId)).transfer(tokenPrice[tokenId]);
+        transferFrom(msg.sender, ownerOf(tokenId), tokenId);
+    }
+
+    SubmittedInfo[] public submitList;
+
+    function submitTokens (SubmittedInfo memory submittedItem) public {
+        submitList.push(submittedItem);
+    }
+
+    function getSubmittedList() public view returns (SubmittedInfo[] memory){
+        return submitList;
     }
 
 
@@ -120,4 +170,11 @@ struct BigPictureData {
 struct TokenData {
     uint256 id;
     string image;
+    bool onSale;
+    uint256 tokenPrice;
+}
+
+struct SubmittedInfo {
+    address owner;
+    address[] tokenList;
 }
